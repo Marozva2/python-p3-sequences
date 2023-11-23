04b4a8960d5b69bd564847a8b1180d44dd249036
Marozva <isabokefrankline@gmail.com>
2023-11-23 17:25:04 +0300

    completed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print('[]')
+    elif length == 1:
+        print('[0]')
+    elif length == 2:
+        print('[0, 1]')
+    else:
+        list = [0, 1]
+        while len(list) < length:
+            list.append(list[-1] + list[-2])
+       # print('[{}]'.format(', '.join(map(str, list))))
